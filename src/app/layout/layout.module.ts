f1acbbbfc1f9548877cca3f7193ad5ceebba7d34
lukkerr <luciano_carvalho@outlook.com.br>
2021-08-22 22:19:13 -0300

    4 files modified

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,16 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button/button-module';
 
 @NgModule({
   declarations: [MenuComponent],
   imports: [
     BrowserAnimationsModule,
-    CommonModule,
     MatButtonModule,
-    MatMenuModule
-  ]
+    MatMenuModule,
+    CommonModule
+  ],
+  exports: [MenuComponent]
 })
 export class LayoutModule {}
